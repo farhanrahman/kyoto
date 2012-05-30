f860f760a2aea5dcddd0af8b5961995ae7451ca5
Chris Darby <christopher.darby08@imperial.ac.uk>
2012-05-30 17:04:51 +0100

    fleshed out public offer class and added simulation parameters

--- kyoto/src/main/java/uk/ac/ic/kyoto/trade/PublicOffer.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/trade/PublicOffer.java
@@ -3,7 +3,30 @@ package uk.ac.ic.kyoto.trade;
 import uk.ac.imperial.presage2.core.Action;
 
 // Action object for advertising that you want to buy/sell
-public class PublicOffer implements Action {
+public final class PublicOffer implements Action {
 
+	public enum TradeType{
+		BUY, SELL
+	}
 
+	private final TradeType type;
+	private final int quantity;
+	private final int unitPrice;
+
+	public PublicOffer(TradeType type, int quantity, int unitPrice) {
+		this.type = type;
+		this.quantity = quantity;
+		this.unitPrice = unitPrice;
+	}
+	
+	public TradeType getType() {
+		return type;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+	
 }
