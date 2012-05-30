2a34dccaa9e74ce5d88cd28e1fa728ea982c3b0d
Chris Darby <christopher.darby08@imperial.ac.uk>
2012-05-30 17:13:29 +0100

    More work on initiate trade and public offer action objects

--- kyoto/src/main/java/uk/ac/ic/kyoto/trade/PublicOffer.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/trade/PublicOffer.java
@@ -1,6 +1,7 @@
 package uk.ac.ic.kyoto.trade;
 
 import uk.ac.imperial.presage2.core.Action;
+import uk.ac.imperial.presage2.util.participant.AbstractParticipant;
 
 // Action object for advertising that you want to buy/sell
 public final class PublicOffer implements Action {
@@ -12,11 +13,13 @@ public final class PublicOffer implements Action {
 	private final TradeType type;
 	private final int quantity;
 	private final int unitPrice;
+	private final AbstractParticipant owner;
 
-	public PublicOffer(TradeType type, int quantity, int unitPrice) {
+	public PublicOffer(TradeType type, int quantity, int unitPrice, AbstractParticipant owner) {
 		this.type = type;
 		this.quantity = quantity;
 		this.unitPrice = unitPrice;
+		this.owner = owner;
 	}
 	
 	public TradeType getType() {
@@ -27,6 +30,9 @@ public final class PublicOffer implements Action {
 	}
 	public int getUnitPrice() {
 		return unitPrice;
+	}
+	public AbstractParticipant getOwner(){
+		return owner;
 	}
 	
 }
