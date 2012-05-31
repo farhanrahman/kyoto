28fb9ef482d32143a5e5a69f1d27e302a16ca93c
Farhan Rahman <farhanrahman26@gmail.com>
2012-05-31 23:11:41 +0100

    Added actions for Submitting reports. Made the reports as shared state that is available for every agents.

--- kyoto/src/main/java/uk/ac/ic/kyoto/carbon/CarbonReportingService.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/carbon/CarbonReportingService.java
@@ -1,27 +1,53 @@
 package uk.ac.ic.kyoto.carbon;
 
+import java.io.Serializable;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.log4j.Logger;
+
+import uk.ac.imperial.presage2.core.Time;
 import uk.ac.imperial.presage2.core.environment.EnvironmentRegistrationRequest;
 import uk.ac.imperial.presage2.core.environment.EnvironmentService;
 import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
-/**
- * Use this class to alter the Total Carbon reserve present in the environment
- * @author farhanrahman
- *
- */
+import uk.ac.imperial.presage2.core.environment.StateTransformer;
+
+import com.google.inject.Inject;
+
+
 public class CarbonReportingService extends EnvironmentService {
 
+	Logger logger = Logger.getLogger(CarbonReportingService.class);
+	
+	
+	@Inject
 	protected CarbonReportingService(EnvironmentSharedStateAccess sharedState) {
 		super(sharedState);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
 	public void registerParticipant(EnvironmentRegistrationRequest req){
 		super.registerParticipant(req);
-		//TODO extra stuff required when participant registers?
 	}
-	
-	
-	
 
+	public void updateReport(final UUID id, final Double carbonEmission, final Time simTime){
+		/*@SuppressWarnings("unchecked")
+		Map<Time, Double> s = (Map<Time,Double>) this.sharedState.get("Report", id);
+		
+		for(Time key : s.keySet()){
+			logger.info("Key: " + key + " Value: " + s.get(key) + "\n");
+		}*/
+		
+		
+		
+		this.sharedState.change("Report", id, new StateTransformer(){
+			@Override
+			public Serializable transform(Serializable state) {
+				@SuppressWarnings("unchecked")
+				Map<Time,Double> s = (Map<Time,Double>) state;
+				s.put(simTime, carbonEmission);
+				return (Serializable) s;
+			}
+		});
+	}
 }
