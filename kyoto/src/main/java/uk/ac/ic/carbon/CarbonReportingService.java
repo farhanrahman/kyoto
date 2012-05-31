43f99908b9a374270f056dcb8bafdd6476c883bc
Farhan Rahman <farhanrahman26@gmail.com>
2012-05-31 17:18:28 +0100

    Merged with Group 1. Started writing CarbonReportingService for reporting to Global Carbon Reserve

---
+++ kyoto/src/main/java/uk/ac/ic/carbon/CarbonReportingService.java
@@ -0,0 +1,25 @@
+package uk.ac.ic.carbon;
+
+import uk.ac.imperial.presage2.core.environment.EnvironmentRegistrationRequest;
+import uk.ac.imperial.presage2.core.environment.EnvironmentService;
+import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
+/**
+ * Use this class to alter the Total Carbon reserve present in the environment
+ * @author farhanrahman
+ *
+ */
+public class CarbonReportingService extends EnvironmentService {
+
+	protected CarbonReportingService(EnvironmentSharedStateAccess sharedState) {
+		super(sharedState);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void registerParticipant(EnvironmentRegistrationRequest req){
+		
+	}
+	
+	
+
+}
