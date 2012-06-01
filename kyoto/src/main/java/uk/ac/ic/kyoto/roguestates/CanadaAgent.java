40c02d5a13b0e5218a433aadb6625090b5861468
Stuart Holland <soulc3@gmail.com>
2012-06-01 16:51:33 +0100

    Implementing TradeProtocol in CanadaAgent

--- kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/CanadaAgent.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/CanadaAgent.java
@@ -3,9 +3,14 @@ package uk.ac.ic.kyoto.roguestates;
 import java.util.UUID;
 
 import uk.ac.ic.kyoto.countries.NonParticipant;
+import uk.ac.ic.kyoto.trade.TradeProtocol;
 import uk.ac.imperial.presage2.core.messaging.Input;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+import uk.ac.imperial.presage2.util.fsm.FSMException;
 
 public class CanadaAgent extends NonParticipant {
+	
+	TradeProtocol tradeProtocol;
 
 	public CanadaAgent(UUID id, String name,String ISO, double landArea, double arableLandArea, double GDP,
 			double GDPRate, long emissionsTarget, long carbonOffset,
@@ -25,8 +30,20 @@ public class CanadaAgent extends NonParticipant {
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
-		if (carbonOutput - emissionsTarget + carbonOffset > 0) {
-			
+		try {
+			tradeProtocol = new TradeProtocol(getID(), authkey, environment, network) {
+				@Override
+				protected boolean acceptExchange(NetworkAddress from,
+						Trade trade) {
+					if (carbonOutput - emissionsTarget + carbonOffset > 0) {
+						return true;
+					}
+					return false;
+				}
+			};
+		} catch (FSMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
