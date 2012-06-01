45ec4d68c41855e13d75dfeeebbc1274988f7799
Chris Tory <christory@hotmail.com>
2012-06-01 13:59:43 +0100

    Added gameConst

---
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/GameConst.java
@@ -0,0 +1,11 @@
+package uk.ac.ic.kyoto.countries;
+
+public final class GameConst {
+	// GDP Growth Values
+	public static final double GROWTH_MARKET_STATE = 0.05;
+	public static final double STABLE_MARKET_STATE = 0.02;
+	public static final double RECESSION_MARKET_STATE = 0.03;
+	public static final int ECONOMIC_OUTPUT_REDUCTION = 1;
+	
+	//
+}
