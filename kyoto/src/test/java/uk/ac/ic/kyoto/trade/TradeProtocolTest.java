50642c88a16a903f471231180eaa626ba7999077
Chris Darby <christopher.darby08@imperial.ac.uk>
2012-06-01 16:56:10 +0100

    TODO in TradeProtocolTest

--- kyoto/src/test/java/uk/ac/ic/kyoto/trade/TradeProtocolTest.java
+++ kyoto/src/test/java/uk/ac/ic/kyoto/trade/TradeProtocolTest.java
@@ -5,6 +5,9 @@ package uk.ac.ic.kyoto.trade;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import uk.ac.ic.kyoto.trade.TradeProtocol.Trade;
+import uk.ac.imperial.presage2.core.network.NetworkAddress;
+import uk.ac.imperial.presage2.util.fsm.FSMException;
 
 /**
  * @author cmd08
@@ -14,6 +17,7 @@ public class TradeProtocolTest {
 
 	@Test
 	public void test() {
+		/* Test the Trade class first*/
 		int quantity = 10;
 		int unitCost = 10;
 		TradeProtocol.Trade buy = new TradeProtocol.Trade(quantity, unitCost, TradeType.BUY);
@@ -24,6 +28,26 @@ public class TradeProtocolTest {
 		assertTrue(buy.reverse().reverse().equals(buy));
 		assertTrue(sell.reverse().equals(buy));
 		
+		
+		/* 
+		 * Test the protocol - Need an agent, environment and network to test this class :(
+		 */
+		
+//		TradeProtocol protocol;
+//		
+//		try {
+//			protocol = new TradeProtocol(null, null, null, null) {
+//				
+//				@Override
+//				protected boolean acceptExchange(NetworkAddress from, Trade trade) {
+//					// TODO Auto-generated method stub
+//					return false;
+//				}
+//			};
+//		} catch (FSMException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		
 	}
 
