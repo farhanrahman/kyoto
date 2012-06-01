d7e25ed7258117bb9b4d980ae30bfe28b9a11afc
Chris Tory <christory@hotmail.com>
2012-06-01 14:03:12 +0100

    Added GDP percentage

--- kyoto/src/main/java/uk/ac/ic/kyoto/countries/GameConst.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/GameConst.java
@@ -1,11 +1,21 @@
 package uk.ac.ic.kyoto.countries;
 
 public final class GameConst {
+	
 	// GDP Growth Values
 	public static final double GROWTH_MARKET_STATE = 0.05;
 	public static final double STABLE_MARKET_STATE = 0.02;
 	public static final double RECESSION_MARKET_STATE = 0.03;
 	public static final int ECONOMIC_OUTPUT_REDUCTION = 1;
+	public static final int GROWTH_SCALER = 1;
 	
-	//
+	//GDP - percentage of GDP availaible to spend
+	public static final double PERCENTAGE_OF_GDP = 0.1;
+	
+	
+	//Carbon Absorbtion
+	
+	
+	//Country Config Template
+	
 }
