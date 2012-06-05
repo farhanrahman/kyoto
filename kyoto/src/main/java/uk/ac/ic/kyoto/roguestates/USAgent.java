8d1422f27b60e6a293ef90271a1386fdd9c60ad5
Stuart Holland <soulc3@gmail.com>
2012-06-05 16:08:11 +0100

    Tweaking constructors again

--- kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/USAgent.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/USAgent.java
@@ -2,17 +2,16 @@ package uk.ac.ic.kyoto.roguestates;
 
 import java.util.UUID;
 
-import uk.ac.ic.kyoto.countries.NonParticipant;
 
 public class USAgent extends NonParticipant {
 
 
 	public USAgent(UUID id, String name,String ISO, double landArea, double arableLandArea, double GDP,
 			double GDPRate, float availableToSpend, long emissionsTarget, long carbonOffset,
-			long energyOutput){
+			long energyOutput, long carbonOutput){
 		super(id, name, ISO, landArea, arableLandArea, GDP,
 				GDPRate, availableToSpend, emissionsTarget, carbonOffset,
-				energyOutput);
+				energyOutput, carbonOutput);
 		// TODO Auto-generated constructor stub
 	}
 	
