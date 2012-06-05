af2a2c6459d88496f680abacb5636cfebd614c90
Stuart Holland <soulc3@gmail.com>
2012-06-05 16:27:00 +0100

    Initial implementation of election system for US

--- kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/USAgent.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/roguestates/USAgent.java
@@ -2,9 +2,16 @@ package uk.ac.ic.kyoto.roguestates;
 
 import java.util.UUID;
 
+import uk.ac.ic.kyoto.services.TimeService.EndOfYear;
+import uk.ac.imperial.presage2.core.event.EventListener;
+import uk.ac.imperial.presage2.core.util.random.Random;
+
 
 public class USAgent extends NonParticipant {
 
+	private int yearMod4 = 0;
+	
+	private boolean democratElected=true;
 
 	public USAgent(UUID id, String name,String ISO, double landArea, double arableLandArea, double GDP,
 			double GDPRate, float availableToSpend, long emissionsTarget, long carbonOffset,
@@ -13,6 +20,25 @@ public class USAgent extends NonParticipant {
 				GDPRate, availableToSpend, emissionsTarget, carbonOffset,
 				energyOutput, carbonOutput);
 		// TODO Auto-generated constructor stub
+	}
+	
+	@EventListener
+	private void yearCounter(EndOfYear e) {
+		yearMod4++;
+		if (yearMod4 == 4) {
+			yearMod4 = 0;
+			election();
+		}
+	}
+	
+	private void election() {
+		int rand = Random.randomInt(100);
+		if (rand < 50) {
+			democratElected = true;
+		}
+		else {
+			democratElected = false;
+		}
 	}
 	
 	@Override
