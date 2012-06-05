f8e8274bca036dce376617f10491edeae15393bb
azyzio <zysko.adam@gmail.com>
2012-06-05 15:42:44 +0100

    Adding comments

--- kyoto/src/main/java/uk/ac/ic/kyoto/countries/AbstractCountry.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/AbstractCountry.java
@@ -157,6 +157,12 @@ public abstract class AbstractCountry extends AbstractParticipant {
 		return new Double(carbonEmission);
 	}
 	
+
+	
+	//================================================================================
+    // Energy Output Control functions
+    //================================================================================
+	
 	/**
 	 * Reduces both the energyOutput and carbonOutput of the country
 	 * It can be used to limit carbonOuput without any financial cost
@@ -167,11 +173,6 @@ public abstract class AbstractCountry extends AbstractParticipant {
 	 * Amount of energyOuput that should be reduced
 	 * It has to be positive and lower than the total carbonOuput
 	 */
-	
-	//================================================================================
-    // Investing in 
-    //================================================================================
-	
 	protected void reduceEnergyOutput (long amount) throws IllegalArgumentException{
 		if (amount < carbonOutput && amount > 0) {
 			energyOutput -= amount;
@@ -181,20 +182,45 @@ public abstract class AbstractCountry extends AbstractParticipant {
 			throw new IllegalArgumentException("Specified amount should be > 0 and < carbonOutput");
 	}
 	
+	/**
+	 * Calculates the cost of investing in carbon industry
+	 * @param carbon
+	 * The expected increase in carbon output
+	 * @return
+	 * The cost for the country
+	 */
 	protected long calculateCostOfInvestingInCarbonIndustry (long carbon){
 		return (long) (carbon * GameConst.CARBON_INVESTMENT_PRICE);
 	}
 	
+	/**
+	 * Calculates the increase of carbon output
+	 * @param cost
+	 * The amount of money to be spent on carbon industry growth
+	 * @return
+	 * The increase of carbon output
+	 */
 	protected long calculateCarbonIndustryGrowth (long cost){
 		return (long) (cost / GameConst.CARBON_INVESTMENT_PRICE);
 	}
 	
+	/**
+	 * Invests in carbon industry.
+	 * Carbon output and energy output of the country go up
+	 * @param carbon
+	 * The increase of the carbon output that will be achieved.
+	 */
 	protected void investInCarbonIndustry(long carbon){
 		try {
 			long cost = calculateCostOfInvestingInCarbonIndustry(carbon);
-			carbonOutput += carbon;
-			energyOutput += carbon;
-			availableToSpend -= cost;
+			if (cost > availableToSpend) {
+				carbonOutput += carbon;
+				energyOutput += carbon;
+				availableToSpend -= cost;
+			}
+			else {
+				// log that there is not enough money
+			}
 		}
 		catch (Exception e) {
 			// log the exception
