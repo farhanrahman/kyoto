437de685b1a9879e400a3530c350ab681513091f
gpilikos@gmail.com <george@george-Aspire-S3>
2012-06-06 14:41:41 +0100

    Abstract country testing

--- kyoto/src/test/java/uk/ac/ic/kyoto/countries/AbstractCountryTest.java
+++ kyoto/src/test/java/uk/ac/ic/kyoto/countries/AbstractCountryTest.java
@@ -17,12 +17,13 @@ public class AbstractCountryTest {
 		TestCountry tester = new TestCountry(randomID, "Poland", "ISO", 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000);
 
 		assertTrue(tester.getCash() == 1000*GameConst.PERCENTAGE_OF_GDP);
-		
+		assertTrue(tester.calculateCostOfInvestingInCarbonIndustry(10) == 10 * GameConst.CARBON_INVESTMENT_PRICE );
 	}
 	
-	@Test
-	public void carbonReductionHandlerTest()
-	{
-		assertTrue(tester.carbonReductionHandler.getCarbonOutputChange(1000) == 1000*1000/GameConst.CARBON_REDUCTION_COEFF);
-	}
+	//@Test
+	//public void carbonRe/ductionHandlerTest()
+	//{
+		//assertTrue(tester.carbonReductionHandler.getCarbonOutputChange(1000) == 1000*1000/GameConst.CARBON_REDUCTION_COEFF);
+//}
+		
 }
