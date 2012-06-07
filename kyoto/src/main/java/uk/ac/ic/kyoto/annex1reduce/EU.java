4cdfc585cf74efbfcff9140aa854af71f3a130f9
cs2309 <chris.socha09@imperial.ac.uk>
2012-06-07 11:35:42 +0100

    Changed EU to EnvironmentService, and fixed JavaDocs

--- kyoto/src/main/java/uk/ac/ic/kyoto/annex1reduce/EU.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/annex1reduce/EU.java
@@ -1,78 +1,9 @@
 package uk.ac.ic.kyoto.annex1reduce;
 
-import java.util.ArrayList;
-import java.util.UUID;
+import uk.ac.imperial.presage2.core.environment.EnvironmentService;
 
-import uk.ac.imperial.presage2.core.messaging.Input;
-import uk.ac.imperial.presage2.util.participant.AbstractParticipant;
+public class EU extends EnvironmentService {
+	
 
-// TODO Everything
-
-/**
- * Main EU body
- * Provides functions for allocating credits and imposing sactions on EU members
- * Singleton class with mostly static variables and methods
- * @author Nik
- *
- */
-public class EU extends AbstractParticipant {
-	
-	static final private UUID id = UUID.randomUUID();
-	static final private String NAME = "EuropeanUnion";
-	static final private ArrayList<EUCountry> memberStates = new ArrayList<EUCountry>();
-	
-	public EU() {
-		super(id,NAME);
-	}
-	
-	//TODO PLACEHOLDER Determine how input names work
-	static final String NEWROUND = "NEWROUND";
-	
-	/**
-	 * Called by execute multiple times for many inputs
-	 * @param input The current input to be handled
-	 */
-	@Override
-	protected void processInput(Input input) {
-		String name = input.getType();
-
-		//If the input is relevant, perform an action
-		if (name == NEWROUND) {
-			applySanctions();
-			allocateCredits();
-		}
-		//TODO handle more actions
-		
-		
-	}
-
-	/**
-	 * Called when a round ends. Calculates and applies sanctions to 
-	 * member states
-	 */
-	static private void applySanctions() {
-		for (EUCountry state : memberStates) {
-			//TODO calculate and apply sanctions to member states
-		}
-	}
-	
-	/**
-	 * Called when a round ends. Calculates and allocates credits to
-	 * member states
-	 */
-	static private void allocateCredits() {
-		for (EUCountry state : memberStates) {
-			//TODO Calculate credits to give to all EU member states
-		}
-	}
-	
-	/**
-	 * Add member states to the EU. Allows operation of sanctions, 
-	 * credits, etc.
-	 * @param state 
-	 */
-	static public void addMemberState(EUCountry state) {
-		memberStates.add(state);
-	}
 	
 }
