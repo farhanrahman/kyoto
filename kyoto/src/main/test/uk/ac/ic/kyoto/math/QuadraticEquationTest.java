2fa9d810691f994e4c01c09c3fc4c7db34439c28
Nicolas Khadivi <nicolaskhadivi@gmail.com>
2012-06-08 15:00:45 +0100

    Initial commit

---
+++ kyoto/src/main/test/uk/ac/ic/kyoto/math/QuadraticEquationTest.java
@@ -0,0 +1,14 @@
+package uk.ac.ic.kyoto.math;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuadraticEquationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
