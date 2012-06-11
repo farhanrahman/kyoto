d814d396048f17bed38714c20775315f7e87d4c4
George <gpilikos@gmail.com>
2012-06-11 17:09:53 +0100

    Minor changes to BIC countries, moved Constants in different class

--- kyoto/src/main/java/uk/ac/ic/kyoto/nonannexone/BIC.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/nonannexone/BIC.java
@@ -17,29 +17,14 @@ public class BIC extends AbstractCountry {
 	protected double GDP_aim; //aim of GDP for a year
 	protected long tree_area; // number of trees
 	
-	//Constants.............................................................................
-	
-	public static final long  nf_en_output = 0; //effect on energy output of normal factory(to be decided)
-	public static final long  ef_en_output=0; //environment friendly factory's effect(to be decided)
-	public static final long  NFactorycost=0; //cost of normal factory
-	public static final long EFactorycost=0; //cost of environmental factory (expensive)
-	public static final long carbon_effect_nf = 0; //effect on carbon output
-	public static final long carbon_effect_ef = 0; //effect on carbon output - less than normal factory
-	public static final long war_industry_unit_cost = 0; // cost of creating a unit of the greatest empire ever in the known world
-	public static final long enough_units_for_war = 0; // constant for estimating if in good position for declaring war.
-	public static final long tree_cost = 0 ; //cost of each tree from availableToSpend
-	public static final long tree_effect_on_carbon_output = 0; //reduction in carbon output
-	
-	
-	//............................................................................................ 
+		//............................................................................................ 
 	
 	public BIC(UUID id, String name, String ISO, double landArea, double arableLandArea, double GDP,
-			double GDPRate, long emissionsTarget, long energyOutput, long carbonOutput)
+			double GDPRate, long availableToSpend, long emissionsTarget, long carbonOffset,
+			long energyOutput, long carbonOutput)
 	{
-		super(id, name, ISO, landArea, arableLandArea, GDP,
-				GDPRate, emissionsTarget,
-				energyOutput, carbonOutput);
-
+		super(id, name, ISO, landArea, arableLandArea, GDP, GDPRate, availableToSpend, emissionsTarget, carbonOffset, energyOutput, carbonOutput);
+		
 }
 	
 	//Inherited functions......................................................................
