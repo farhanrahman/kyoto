4d4be2f517acf08141dc4e540cd87b7420aa2901
George <gpilikos@gmail.com>
2012-06-13 21:04:29 +0100

    Environment care or not behavior added

--- kyoto/src/main/java/uk/ac/ic/kyoto/nonannexone/CountryConstants.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/nonannexone/CountryConstants.java
@@ -2,5 +2,7 @@ package uk.ac.ic.kyoto.nonannexone;
 
 public final class CountryConstants {
 	
+public static final double ENERGY_AIM_GROWTH = 0.125;
+public static final double TARGET_AIM_GROWTH = 0.1;
 	
 }
