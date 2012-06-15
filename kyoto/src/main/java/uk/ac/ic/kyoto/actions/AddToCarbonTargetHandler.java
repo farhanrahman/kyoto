178352ebee4b478e77209b4c091c6a86e235b3b8
Jonathan Ely <me@jonathanely.com>
2012-06-15 14:35:48 +0100

    Implemented action to adding country to CarbonTarget

---
+++ kyoto/src/main/java/uk/ac/ic/kyoto/actions/AddToCarbonTargetHandler.java
@@ -0,0 +1,42 @@
+package uk.ac.ic.kyoto.actions;
+
+import java.util.UUID;
+
+import uk.ac.ic.kyoto.countries.CarbonTarget;
+import uk.ac.imperial.presage2.core.Action;
+import uk.ac.imperial.presage2.core.environment.ActionHandler;
+import uk.ac.imperial.presage2.core.environment.ActionHandlingException;
+import uk.ac.imperial.presage2.core.environment.EnvironmentServiceProvider;
+import uk.ac.imperial.presage2.core.environment.EnvironmentSharedStateAccess;
+import uk.ac.imperial.presage2.core.environment.UnavailableServiceException;
+import uk.ac.imperial.presage2.core.messaging.Input;
+
+import com.google.inject.Inject;
+
+/**
+ * Action object handler for adding country to CarbonTarget service
+ * 
+ * @author Jonathan Ely
+ */
+public class AddToCarbonTargetHandler implements ActionHandler {
+
+	final protected CarbonTarget ct;
+	
+	@Inject
+	public AddToCarbonTargetHandler (EnvironmentSharedStateAccess sharedState, EnvironmentServiceProvider environment) throws UnavailableServiceException {
+		this.ct = environment.getEnvironmentService(CarbonTarget.class);
+	}
+	
+	@Override
+	public boolean canHandle(Action action) {
+		return action instanceof AddToCarbonTarget;
+	}
+
+	@Override
+	public Input handle(Action action, UUID actor) throws ActionHandlingException {
+		AddToCarbonTarget obj = (AddToCarbonTarget) action;
+		ct.addMemberState(obj.countryObject);
+		return null;
+	}
+
+}
