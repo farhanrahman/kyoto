0042f2d5613a7183ba6449599fd5b7fabfa19216
Nikunj Patel <np1509@gmail.com>
2012-06-17 14:01:30 +0100

    Fixed handlers, cleaned up some other stuff

--- kyoto/src/main/java/uk/ac/ic/kyoto/annex1reduce/AnnexOneReduce.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/annex1reduce/AnnexOneReduce.java
@@ -4,11 +4,6 @@ import java.util.UUID;
 import uk.ac.ic.kyoto.countries.AbstractCountry;
 import uk.ac.imperial.presage2.core.environment.UnavailableServiceException;
 import uk.ac.imperial.presage2.core.messaging.Input;
-import alice.tuprolog.MalformedGoalException;
-import alice.tuprolog.NoSolutionException;
-import alice.tuprolog.Prolog;
-import alice.tuprolog.SolveInfo;
-import alice.tuprolog.UnknownVarException;
 
 /**
  * Extends AbstractCountry, provides a skeleton for all EU member countries
@@ -17,13 +12,11 @@ import alice.tuprolog.UnknownVarException;
  */
 public class AnnexOneReduce extends AbstractCountry {
 	
-	final private Prolog engine;
 	private EU eu;
 	
 	public AnnexOneReduce(UUID id, String name,String ISO, double landArea, double arableLandArea, double GDP, double GDPRate, double energyOutput, 
 			double carbonOutput) {
 		super(id, name, ISO, landArea, arableLandArea, GDP, GDPRate, energyOutput, carbonOutput);
-		engine = EUBehaviours.getEngine(name);
 	}
 
 	@Override
@@ -48,51 +41,7 @@ public class AnnexOneReduce extends AbstractCountry {
 	
 	@Override
 	protected void behaviour() {
-		
-		//TODO perform analysis
-		
-		//TODO call prolog functions
-		String term = evaluateString("basicTest(test,X).","X");
-		System.out.println(term);
-	}
-	
-	/**
-	 * Pass in a string for the loaded Prolog engine to evaulate and the term
-	 * we want returned. Will return a maximum of one term containing the action
-	 * that is to be taken by the country
-	 * 
-	 * @param input The string to evaluate
-	 * @param term The term to return
-	 * @return A string containing the returned term
-	 */
-	private String evaluateString(String input, String term){
-		
-		SolveInfo info = null;
-		try {
-			info = engine.solve(input);
-		} catch (MalformedGoalException e) {
-			e.printStackTrace();
-		}
-		
-		String output;
-		
-		if (info.isSuccess()) {
-			try {
-				output = info.getTerm(term).toString();
-			} catch (NoSolutionException e) {
-				e.printStackTrace();
-				output = null;
-			} catch (UnknownVarException e) {
-				e.printStackTrace();
-				output = null;
-			}
-		}
-		else {
-			output = "false";
-		}
-		
-		return output;
-		
+		//TODO
 	}
 
 	@Override
