8d318b1fa65310359c3ffbbab7b9b2d6bc04c1ba
Chris Tory <christory@hotmail.com>
2012-06-17 18:13:20 +0100

    Moved fossil prices, removed market

--- kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/GDPTestCount.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/GDPTestCount.java
@@ -23,6 +23,7 @@ public class GDPTestCount extends AbstractCountry {
 		logger.debug("Current Cash: " + this.getAvailableToSpend());
 		logger.debug("Current GDP Rate: " + this.getGDPRate());
 		logger.debug("Current Energy Output: " + this.getEnergyOutput());
+		logger.debug("Current Previous Energy Output: " + this.getPrevEnergyOutput());
 		logger.debug("Current CO2 Output: " + this.getCarbonOutput());
 
 	}
