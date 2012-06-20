1ddeab796d9583dc673898731604e644f71ce3a1
Chris Darby <christopher.darby08@imperial.ac.uk>
2012-06-20 11:01:45 +0100

    rewritten try/catch to be more sensible

--- kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/Reduce.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/Reduce.java
@@ -3,6 +3,8 @@ package uk.ac.ic.kyoto.countries.testCountries;
 import java.util.UUID;
 import uk.ac.ic.kyoto.countries.AbstractCountry;
 import uk.ac.ic.kyoto.countries.Offer;
+import uk.ac.ic.kyoto.exceptions.NotEnoughCarbonOutputException;
+import uk.ac.ic.kyoto.exceptions.NotEnoughCashException;
 import uk.ac.imperial.presage2.core.messaging.Input;
 import uk.ac.imperial.presage2.core.network.NetworkAddress;
 
@@ -34,16 +36,23 @@ public class Reduce extends AbstractCountry {
 		 * Attempt to minimise carbon output, spend all available money on carbon absorption & reduction
 		 */
 		logger.debug(this.getName());
-		try {
-			logger.debug("Available Cash: " + getAvailableToSpend());
-			logger.debug("CO2 Reduction Max: "+ carbonReductionHandler.getCarbonOutputChange(getAvailableToSpend()));
-			double reduction = carbonReductionHandler.getCarbonOutputChange(getAvailableToSpend()-100);
-			logger.debug("Attempting reduction of: "+reduction+" Tonnes");
-			carbonReductionHandler.investInCarbonReduction(reduction);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		logger.debug("Available Cash: " + getAvailableToSpend());
+		logger.debug("CO2 Reduction Max: "+ carbonReductionHandler.getCarbonOutputChange(getAvailableToSpend()));
 		
+		double reduction = carbonReductionHandler.getCarbonOutputChange(getAvailableToSpend());
+		
+		logger.debug("Attempting reduction of: "+reduction+" Tonnes");
+		
+		try {
+			carbonReductionHandler.investInCarbonReduction(reduction);
+			/*These should never ever be thrown happen, we did a runtime check above */
+		} catch (NotEnoughCarbonOutputException e) {
+			throw new RuntimeException(e);
+		} catch (NotEnoughCashException e) {
+			throw new RuntimeException(e);
+		}
+
 		logger.debug("Current GDP: " + this.getGDP());
 		logger.debug("Current Energy Output: " + this.getEnergyOutput());
 		logger.debug("Current CO2 Output: " + this.getCarbonOutput());
