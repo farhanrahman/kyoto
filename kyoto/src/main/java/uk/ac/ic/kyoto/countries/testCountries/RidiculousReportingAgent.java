446636633ad1642ff839b7278e5bb0299c98bf8b
Chris Darby <christopher.darby08@imperial.ac.uk>
2012-06-22 12:25:51 +0100

    Ugly commit to put all test classes in the right place and fix more compile errors

--- kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/RidiculousReportingAgent.java
+++ kyoto/src/main/java/uk/ac/ic/kyoto/countries/testCountries/RidiculousReportingAgent.java
@@ -1,28 +1,32 @@
-package uk.ac.ic.kyoto.testagents;
+package uk.ac.ic.kyoto.countries.testCountries;
 
 import java.util.UUID;
 
-import uk.ac.ic.kyoto.actions.SubmitCarbonEmissionReport;
 import uk.ac.ic.kyoto.countries.AbstractCountry;
 import uk.ac.ic.kyoto.countries.Offer;
-import uk.ac.imperial.presage2.core.environment.ActionHandlingException;
+import uk.ac.imperial.presage2.core.Time;
 import uk.ac.imperial.presage2.core.messaging.Input;
 import uk.ac.imperial.presage2.core.network.NetworkAddress;
-import uk.ac.imperial.presage2.core.simulator.SimTime;
 
-public class CheatingAgent extends AbstractCountry {
+public class RidiculousReportingAgent extends AbstractCountry {
 
-	public CheatingAgent(UUID id, String name, String ISO, double landArea,
-			double arableLandArea, double GDP, double GDPRate,
+	public RidiculousReportingAgent(UUID id, String name, String ISO) {
+		super(id, name, ISO);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RidiculousReportingAgent(UUID id, String name, String ISO,
+			double landArea, double arableLandArea, double GDP, double GDPRate,
 			double energyOutput, double carbonOutput) {
 		super(id, name, ISO, landArea, arableLandArea, GDP, GDPRate,
 				energyOutput, carbonOutput);
-		// TODO Auto-generated constructor stub
+		setKyotoMemberLevel(KyotoMember.ROGUE);
 	}
 
 	@Override
 	protected void behaviour() {
-		logger.info("Hehe, you'll NEVER catch me");
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -33,13 +37,13 @@ public class CheatingAgent extends AbstractCountry {
 
 	@Override
 	protected void yearlyFunction() {
-		logger.info("NEW YEAR'S!");
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void sessionFunction() {
-		logger.info("NEW SESSION!");
+		// TODO Auto-generated method stub
 
 	}
 
@@ -57,7 +61,7 @@ public class CheatingAgent extends AbstractCountry {
 	
 	@Override
 	protected double getReportedCarbonOutput() {
-		return getCarbonOutput()/2;
+		return getCarbonOutput();
 	}
 
 }
