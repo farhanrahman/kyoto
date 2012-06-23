913aa764b4956e536a2f7f9eb4f3fc27d200f54e
Nikunj Patel <np1509@gmail.com>
2012-06-23 16:28:25 +0100

    Fixed bugs in annexonereduce

--- kyoto/src/main/test/uk/ac/ic/kyoto/annex1reduce/AnnexOneReduceTest.java
+++ kyoto/src/main/test/uk/ac/ic/kyoto/annex1reduce/AnnexOneReduceTest.java
@@ -30,7 +30,7 @@ public class AnnexOneReduceTest {
 		double energyOutput = 703718793;
 		double carbonOutput = 409094000;
 		double availableToSpend = GDP * GameConst.getPercentageOfGdp();
-		double emissionsTarget = 1.01 * carbonOutput;
+		double emissionsTarget = 0.98 * carbonOutput;
 		
 		test = new AnnexOneReduce(UUID.randomUUID(), "JunitTest", "JUT",
 				landArea, arableLandArea, GDP, GDPRate, energyOutput,
@@ -48,7 +48,9 @@ public class AnnexOneReduceTest {
 	@Test
 	public void testbehaviour() {
 
-		test.runSimulation();
+		test.performReduceMaintainActions();
+//		test.behaviour();
+//		test.runSimulation();
 
 	}
 
